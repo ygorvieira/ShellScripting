c3d1eb0230cdc1473d3bb77159e31979f1b39707
Ygor Vieira <ygorus@gmail.com>
2020-10-24 00:41:15 -0300

    Convertendo arquivos via shellScript

---
+++ conversao-jpg-png.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+converte_imagem(){
+cd /mnt/e/ALURA/Linux/ShellScript/imagens-livros
+if [ ! -d png ]
+then
+	mkdir png
+fi
+
+for imagem in *.jpg
+do
+	local imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}') 
+	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
+done
+}
+
+converte_imagem 2>erros_conversao.txt
+
+if [ $? -eq 0 ]
+then
+	echo "Conversao realizada com sucesso"
+else
+	echo "Houve uma falha no processo"
+fi
